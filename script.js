88fd13da051275cc4803eea95f3321f0be32336d
PriyaRajani <piyurajani123@gmail.com>
2025-05-26 16:38:01 +0530

    basic-1

---
+++ script.js
@@ -0,0 +1,55 @@
+const input = document.getElementById('add-input');
+const addBtn = document.getElementById('add-btn');
+const todoCards = document.querySelector('.todo-cards');
+
+let count = 0;
+ function deleteTodo(id){
+    let num = id[4];
+    let contId = 'container-'+num;
+    let ele = document.getElementById(contId)
+    todoCards.removeChild(ele)
+    
+ }
+ function editTodo(id,todo){
+    let num = id[5];
+    let pId = `todo-${num}`;
+    let edId = `edit-${num}`;
+    todo = document.getElementById(pId).textContent;
+    let newTodo =  prompt('enter new todo',todo)
+    if(newTodo === ''){
+      alert('your todo was empty enter again to edit !')
+      return;
+    }
+    if(newTodo !== todo){
+            let para = document.getElementById(pId);
+            para.textContent = newTodo;   
+            
+    }
+ }
+ addBtn.addEventListener("click",()=>{
+         let todo = input.value;
+         if(todo === ''){
+            alert("plz enter your todo first")
+         }else{
+            count++;
+            todoCards.classList.remove("hidden");
+            todoCards.innerHTML += 
+            `
+            <div class ='container' id='container-${count}'>
+            <div class='card'>
+                 <p id='todo-${count}'>${todo}</p> 
+               
+            </div>
+              <button type='button' class='delete-btn btns'id='del-${count}'onclick=deleteTodo('del-${count}')>⛔</button>
+              <button type ='button'  class='edit-btn btns'id='edit-${count}'onclick =editTodo('edit-${count}','${todo}')>✏️</button>
+            <div>
+            </div>
+            `
+        
+         
+         }
+    }) 
+    
+   
+   
+    
